ba16b6c73cd5e210abbb5d8434712b2299420d91
Michael Kim <MichaelKim0717@gmail.com>
2018-08-18 16:06:14 -0400

    Modal creation

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendsArray = [
     {
         name : "Jeff",
-        photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo :"https://vignette.wikia.nocookie.net/althistory/images/7/74/Recent-portraits-random-people-in-random-places_11.jpg/revision/latest?cb=20141209232111",
         scores :
         [
             5,
@@ -15,6 +15,125 @@ var friendsArray = [
             5,
             4,
             1
+        ]
+    },
+    {   
+        name : "Peter",
+        photo :"http://pm1.narvii.com/5866/45cc61eb9e3eaefdc26be045e9f9fbfdf916f45b_00.jpg",
+        scores :
+        [
+            4,
+            2,
+            1,
+            3,
+            2,
+            5,
+            4,
+            4,
+            2,
+            2
+        ]
+    },
+    {
+        name : "Claire",
+        photo : "https://www.rd.com/wp-content/uploads/2017/03/22-People-Share-the-Random-Act-of-Kindness-That-Changed-Their-Life-Polly-380x254.jpg",
+        scores :
+        [
+            3,
+            2,
+            3,
+            3,
+            4,
+            5,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name : "Rachel",
+        photo : "https://dz9yg0snnohlc.cloudfront.net/new-the-enjoyable-art-of-learning-to-talk-to-random-people-2.png",
+        scores :
+        [
+            4,
+            1,
+            3,
+            2,
+            4,
+            3,
+            4,
+            1,
+            5,
+            4
+        ]
+    },
+    {
+        name : "Gary",
+        photo : "http://www.garyparker.com/users/GaryParker3765/largerThumbs/GaryParker37651642786T.jpg",
+        scores :
+        [
+            1,
+            4,
+            2,
+            4,
+            5,
+            5,
+            5,
+            3,
+            1,
+            2
+        ]
+    },
+    {
+        name : "John",
+        photo : "https://secure.gravatar.com/avatar/9f4451afa3ce4be72f4ec8227bd8d320?s=400&d=mm&r=g",
+        scores :
+        [
+            2,
+            2,
+            1,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            5
+        ]
+    },
+    {
+        name : "Rebecca",
+        photo : "https://i.pinimg.com/736x/63/48/b3/6348b3d5a126d45380367bbd12948fc4--tops-people.jpg",
+        scores :
+        [
+            4,
+            5,
+            4,
+            3,
+            2,
+            5,
+            4,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        name : "Lily",
+        photo : "https://cdn-01.belfasttelegraph.co.uk/life/weekend/article35579384.ece/85e51/AUTOCROP/w620h342/2017-04-01_lif_29877253_I2.JPG",
+        scores :
+        [
+            3,
+            5,
+            1,
+            3,
+            5,
+            4,
+            2,
+            5,
+            4,
+            3
         ]
     }
 ]
